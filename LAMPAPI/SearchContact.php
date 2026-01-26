45487d236d531996385d89d679454c07fba6e14d
Nolan Engler <nolan.w.engler@gmail.com>
2026-01-26 14:41:58 -0500

    Update SearchContact.php

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,1 +1,79 @@
+<?php
+
+$inData = getRequestInfo();
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error){
+  
+  returnWithError($conn->connect_error);
+  
+}else{
+   
+  $sql = "SELECT * FROM Contacts 
+          WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) 
+          AND UserID = ?";
+  
+  $stmt = $conn->prepare($sql);
+
+  $searchTerm = "%" . $inData["search"] . "%";
+  $stmt->bind_param("ssssi", $searchTerm, $searchTerm, $searchTerm, $searchTerm, $inData["userId"]);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  $contactsString = "";
+  $count = 0;
+
+  while($row = $result->fetch_assoc()){
+        
+    if($count > 0){
+            
+      $contactsString .= ",";
+        
+    }
+        
+    $contactsString .= '{"id":' . $row["ID"] .
+                          ',"firstName":"' . $row["FirstName"] .
+                          '","lastName":"' . $row["LastName"] .
+                          '","phone":"' . $row["Phone"] .
+                          '","email":"' . $row["Email"] . '"}';
+    $count++;
+    
+  }
+
+  if($count===0){
+      
+    returnWithError("Person Not Found");
+      
+  }else{
+      
+    returnWithInfo($contactsString);
+      
+  }
+
+  $stmt->close();
+  $conn->close();
+
+}
+
+function getRequestInfo(){
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj){
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err){
+    $retValue = '{"results":[],"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($contactsString){
+    $retValue = '{"results":[' . $contactsString . '],"error":""}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
 
