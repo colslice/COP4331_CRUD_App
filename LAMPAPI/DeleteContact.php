6103aa5df0b43e9ade23550d3cc54837f1709752
Nolan Engler <nolan.w.engler@gmail.com>
2026-01-26 14:36:31 -0500

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,1 +1,50 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$contactId = $inData["id"];
+	$userId    = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if ($conn->connect_error){
+    
+		returnWithError($conn->connect_error);
+    
+	}else{
+    
+		$stmt = $conn->prepare(
+			"DELETE FROM Contacts WHERE ID = ? AND UserID = ?"
+		);
+
+		$stmt->bind_param("ii", $contactId, $userId);
+		$stmt->execute();
+
+		returnWithInfo("", "", $contactId);
+
+		$stmt->close();
+		$conn->close();
+    
+	}
+
+	function getRequestInfo(){
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj){
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err){
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo($firstName, $lastName, $id){
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+
+?>
 
