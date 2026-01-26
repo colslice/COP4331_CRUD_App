5c56b3698078fdb784d8cfe0c5345af20cc52dc1
Nolan Engler <nolan.w.engler@gmail.com>
2026-01-26 14:31:02 -0500

    Update CreateContact.php

--- LAMPAPI/CreateContact.php
+++ LAMPAPI/CreateContact.php
@@ -1,1 +1,54 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName  = $inData["lastName"];
+	$phone     = $inData["phone"];
+	$email     = $inData["email"];
+	$userId    = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}else{
+    
+		$stmt = $conn->prepare(
+			"INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID)
+			 VALUES (?, ?, ?, ?, ?)"
+		);
+
+  
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
+		$stmt->execute();
+
+		returnWithInfo($firstName, $lastName, $stmt->insert_id);
+
+		$stmt->close();
+		$conn->close();
+    
+	}
+
+	function getRequestInfo(){
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj){
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err){
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo($firstName, $lastName, $id){
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+
+?>
 
