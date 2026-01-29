645d7877d92ee2cb56823afead59e5f6bc473567
Nolan Engler <nolan.w.engler@gmail.com>
2026-01-28 19:54:42 -0500

    Update Login.php
    fixed a bug by preventing users from logging in with blank values.

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,57 +1,60 @@
 <?php
 
 	$inData = getRequestInfo();
+
+    if($inData['login'] == '' || $inData['password'] == ''){
+            returnWithError("No blank login/password");
+            exit();
+    }
 	
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
 
 	$conn = new mysqli("localhost", "project_user", "COP4331", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
+
+	if($conn->connect_error){
+		
+		returnWithError($conn->connect_error);
+		
+	}else{
+		
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
 		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
-		if( $row = $result->fetch_assoc()  )
-		{
+		if($row = $result->fetch_assoc()){
+			
 			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
-		}
-		else
-		{
+		
+		}else{
+			
 			returnWithError("No Records Found");
+		
 		}
 
 		$stmt->close();
 		$conn->close();
 	}
 	
-	function getRequestInfo()
-	{
+	function getRequestInfo(){
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
+	function sendResultInfoAsJson($obj){
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
-	{
+	function returnWithError($err){
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
 	}
 	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
+	function returnWithInfo($firstName, $lastName, $id){
 		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
 	}
 	
 ?>
