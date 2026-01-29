9198fb64bcbe7d403e883c72997fb271e344a2ab
Nolan Engler <nolan.w.engler@gmail.com>
2026-01-28 19:45:37 -0500

    URL Bug Fix
    fixed a bug where the path for the website login wasn't going to the correct url (line 24)

--- js/code.js
+++ js/code.js
@@ -21,7 +21,7 @@ function doLogin()
 //	var tmp = {login:login,password:hash};
 	let jsonPayload = JSON.stringify( tmp );
 	
-	let url = urlBase + '/Login.' + extension;
+	let url = urlBase + '/LAMPAPI/Login.' + extension;
 
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -183,6 +183,7 @@ function searchColor()
 	}
 	
 }
+
 
 
 
