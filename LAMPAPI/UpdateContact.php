3ac792ef44a57c812f0143b669ad845c16ff879f
Colin Hopkins <131214839+colslice@users.noreply.github.com>
2026-02-04 15:09:12 -0500

    Update database connection credentials in UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -9,8 +9,8 @@ $lastName  = $inData["lastName"];
 $phone     = $inData["phone"];
 $email     = $inData["email"];
 
-	//$conn = new mysqli("localhost", "project_user", "COP4331", "COP4331");
-	$conn = new mysqli("localhost", "root", "", "COP4331");
+	$conn = new mysqli("localhost", "project_user", "COP4331", "COP4331");
+	// $conn = new mysqli("localhost", "root", "", "COP4331");
 
 if($conn->connect_error){
   
