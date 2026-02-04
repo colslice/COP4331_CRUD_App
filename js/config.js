75db60b5c6475001f4ca90cac266b9ba4d22d27b
Colin Hopkins <131214839+colslice@users.noreply.github.com>
2026-02-04 15:06:27 -0500

    Switch API_BASE to production URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-//const API_BASE = "https://cop4331project.xyz/LAMPAPI/";
-const API_BASE = "http://localhost/COP4331_CRUD_App/LAMPAPI/";
+const API_BASE = "https://cop4331project.xyz/LAMPAPI/";
+// const API_BASE = "http://localhost/COP4331_CRUD_App/LAMPAPI/";
 let userId = 0;
 let firstName = "";
 let lastName = "";
