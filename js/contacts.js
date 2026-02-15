ea429a37ce2dd9138b84fc3db11384180db4be53
Sabrina Nguyen <sabinang1809@gmail.com>
2026-02-15 14:22:57 -0500

    fixed bookmark button to fill color when favorite

--- js/contacts.js
+++ js/contacts.js
@@ -98,15 +98,12 @@ function renderContacts() {
 
     <div class="card-actions">
 
-    <button class="fav-btn ${c.favorite == 1 ? "active" : ""}"
+    <button class="fav-btn ${Number(c.favorite) === 1 ? "active" : ""}"
             onclick="toggleFavorite(event, ${c.id})">
 
-        <svg xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            class="fav-icon">
-            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
-        </svg>
-
+      <svg class="fav-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z"/>
+      </svg>
     </button>
 
 
